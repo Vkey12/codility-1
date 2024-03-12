0e86c7c24ff81c839b40be4409bd966a48080b83
Vkey12 <68274999+Vkey12@users.noreply.github.com>
2024-03-12 09:56:05 +0300

    done

--- codility.py
+++ codility.py
@@ -1,29 +1,33 @@
 def solution(A, D):
     balance = 0
-    transactions = {}
-    year = 2020
+    monthly_fees = 5
+    cards = {}
     
-    # to loop through
-    for i in range(len(A)):
-        amount = A[i]
-        date = D[i]
-        transactions[date] = transactions.get(date, 0) + amount
-    
-    # to get the date 
-    for date, amount in transactions.items():
-        year, month, _ = date.split('-')
-        
-        if amount >= 0:
-            balance += amount
+    # using zip to loop
+    for i, date in zip(A, D):
+        month = int(date.split('-') [1])
+        if i >= 0:
+            balance += i
         else:
-            balance += amount
-            
-            if month != '01' and amount < -5:
-                balance -= 5
-    
+            # if negative the balance to be updated
+            balance += i
+            if month in cards:
+                cards[month].append(i)
+            else:
+                cards[month] = [i]
+    # loop through months
+    for month in range(1, 12):
+        if month in cards and len(cards[month]) >= 3 and sum (cards[month]) >= -100:
+            balance += monthly_fees * (len(cards[month]) - 3)
+        else:
+            balance -= monthly_fees
     return balance
 
-# Example usage:
-A1 = [100, 100, 100, -10]
-D1 = ["2020-12-31", "2020-12-22", "2020-12-03", "2020-12-29"]
-print(solution(A1, D1))  
+
+# Example 
+print(solution([100, 100, 100, -10], ["2020-12-31", "2020-12-22", "2020-12-03", "2020-12-29"]))
+
+# year 2020
+# balance 0
+# amount < 0 === card payment && fee = 5
+# amount >= 0 === income transfer
